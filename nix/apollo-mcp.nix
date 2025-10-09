a426642032d857e1724d3d8c7b6d5e5cfc9d6d53
leonid-zats <57621436+leonid-zats@users.noreply.github.com>
2025-10-09 19:37:31 +0300

    Update apollo-mcp.nix

--- nix/apollo-mcp.nix
+++ nix/apollo-mcp.nix
@@ -15,7 +15,9 @@
   graphqlFilter = path: _type: builtins.match ".*graphql$" path != null;
   testFilter = path: _type: builtins.match ".*snap$" path != null;
   srcFilter = path: type:
-    (graphqlFilter path type) || (testFilter path type) || (craneLib.filterCargoSources path type);
+    (graphqlFilter path type)
+    || (testFilter path type)
+    || (craneLib.filterCargoSources path type);
 
   # Crane options
   src = pkgs.lib.cleanSourceWith {
@@ -48,35 +50,38 @@
 
   # Generate a derivation for just the dependencies of the project so that they
   # can be cached across all of the various checks and builders.
-  cargoArtifacts = craneLib.buildDepsOnly (craneCommonArgs // {
-    # Override cargo command to avoid --all-targets which causes cross-compilation
-    cargoCheckCommand = "cargo check --release --locked";
-  });
+  cargoArtifacts = craneLib.buildDepsOnly (
+    craneCommonArgs
+    // {
+      # Override cargo command to avoid --all-targets which causes cross-compilation
+      cargoCheckCommand = "cargo check --release --locked";
+    }
+  );
 in {
   # Expose the list of build dependencies for inheriting in dev shells
   nativeDependencies = craneCommonArgs.nativeBuildInputs;
   dependencies = craneCommonArgs.buildInputs;
 
   # Expose derivations that should be cached in CI
-  cache = [
-    cargoArtifacts
-  ];
+  cache = [cargoArtifacts];
 
   # Expose checks for the project used by the root nix flake
   checks = {
-    clippy = craneLib.cargoClippy (craneCommonArgs
+    clippy = craneLib.cargoClippy (
+      craneCommonArgs
       // {
         inherit cargoArtifacts;
         cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-      });
-    docs = craneLib.cargoDoc (craneCommonArgs
+      }
+    );
+    docs = craneLib.cargoDoc (
+      craneCommonArgs
       // {
         inherit cargoArtifacts;
-      });
+      }
+    );
 
-    rustfmt = craneLib.cargoFmt {
-      inherit src;
-    };
+    rustfmt = craneLib.cargoFmt {inherit src;};
     toml-fmt = craneLib.taploFmt {
       src = pkgs.lib.sources.sourceFilesBySuffices src [".toml"];
     };
@@ -103,16 +108,19 @@ in {
       # Helper for generating a command using cargo-zigbuild and other shell-expanded
       # env vars.
       mkCmd = cmd:
-        builtins.concatStringsSep " " ((lib.optionals stdenv.isDarwin ["SDKROOT=${apple-sdk.sdkroot}"])
+        builtins.concatStringsSep " " (
+          (lib.optionals stdenv.isDarwin ["SDKROOT=${apple-sdk.sdkroot}"])
           ++ [
             "CARGO_ZIGBUILD_CACHE_DIR=$TMP/.cache/cargo-zigbuild"
             "ZIG_LOCAL_CACHE_DIR=$TMP/.cache/zig-local"
             "ZIG_GLOBAL_CACHE_DIR=$TMP/.cache/zig-global"
 
             "${cargo-zigbuild-patched}/bin/cargo-zigbuild ${cmd}"
-          ]);
+          ]
+        );
     in
-      craneLib.buildPackage (craneCommonArgs
+      craneLib.buildPackage (
+        craneCommonArgs
         // {
           pname = craneCommonArgs.pname + "-${target}";
           nativeBuildInputs = [
@@ -128,19 +136,20 @@ in {
 
           # Use zig for both CC and linker since it actually supports cross-compilation
           # nicely.
-          cargoExtraArgs = lib.strings.concatStringsSep " " ([
-              "--target ${zig-target}"
-            ]
+          cargoExtraArgs = lib.strings.concatStringsSep " " (
+            ["--target ${zig-target}"]
             # x86_64-apple-darwin compilation has a bug that causes release builds to
             # fail with "bad relocation", so we build debug targets for it instead.
             # See: https://github.com/rust-cross/cargo-zigbuild/issues/338
-            ++ (lib.optionals (target != "x86_64-apple-darwin") ["--release"]));
+            ++ (lib.optionals (target != "x86_64-apple-darwin") ["--release"])
+          );
 
           cargoCheckCommand = mkCmd "check";
           cargoBuildCommand = mkCmd "zigbuild";
 
           # Make sure to compile it for the specified target
           CARGO_BUILD_TARGET = target;
-        });
+        }
+      );
   };
 }
